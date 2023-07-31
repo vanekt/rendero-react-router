aa93f496293a46d71a5366dc1f5880911c40d2c0
vanekt <vanekt@yandex.ru>
2023-07-31 15:59:22 -0300

    v1.0.1

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link, NavLink, Redirect, Route, Switch } from "react-router-dom";
+
+function initModule() {
+  return {
+    name: "react-router5",
+    components: {
+      router_link: (props, { renderChildren }) => (
+        <Link {...props}>{renderChildren()}</Link>
+      ),
+      router_nav_link: (props, { renderChildren }) => (
+        <NavLink {...props}>{renderChildren()}</NavLink>
+      ),
+      router_redirect: (props, { renderChildren }) => (
+        <Redirect {...props}>{renderChildren()}</Redirect>
+      ),
+      router_route: (props, { renderChildren }) => (
+        <Route {...props}>{renderChildren()}</Route>
+      ),
+      router_switch: (props, { renderChildren }) => (
+        <Switch {...props}>{renderChildren()}</Switch>
+      ),
+    },
+  };
+}
+
+export default initModule;
